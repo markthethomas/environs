07f54e96abf45d05f049bc1c2ea7fcd632a68419
Mark Thomas <markthethomas@gmail.com>
2016-10-01 12:28:24 -0700

    fix: rm crud, add env var getter

--- env.js
+++ env.js
@@ -22,26 +22,35 @@ export const __RELEASE__ = process.env.NODE_ENV === 'production' || process.env.
  * @type {String}
  */
 export const __PRODUCTION__ = process.env.NODE_ENV === 'production';
+
 /**
  * Detect testing environment
  * @type {String}
  */
 export const __TESTING__ = process.env.NODE_ENV === 'testing';
+
 /**
  * Detect dev environment
  * @type {String}
  */
 export const __CI__ = process.env.CI === 'true' || process.env.CI === true;
+
 /**
  * Detect dev environment
  * @type {String}
  */
 export const __DEV__ = process.env.NODE_ENV === 'development';
+
 /**
  * Returns version
  * @type string
  */
 export const __VERSION__ = VERSION;
 
-
-console.log(__VERSION__);
+/**
+ * Get an environment variable
+ * @method getEnv
+ * @param  {string} env var to get
+ * @return {any}     returns variable
+ */
+export const getEnv = (env) => process.env[env];
